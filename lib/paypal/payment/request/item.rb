f1072c582069f48a0cc0ebc43cab3846a9fc4c9e
Corey Reece <corey@storenvy.com>
2012-03-27 16:36:02 -0700

    Add item URL; readability

--- lib/paypal/payment/request/item.rb
+++ lib/paypal/payment/request/item.rb
@@ -1,7 +1,7 @@
 module Paypal
   module Payment
     class Request::Item < Base
-      attr_optional :name, :description, :amount, :quantity, :category
+      attr_optional :name, :description, :amount, :quantity, :category, :url
 
       def initialize(attributes = {})
         super
@@ -10,11 +10,12 @@ module Paypal
 
       def to_params(parent_index, index = 0)
         {
-          :"L_PAYMENTREQUEST_#{parent_index}_NAME#{index}" => self.name,
-          :"L_PAYMENTREQUEST_#{parent_index}_DESC#{index}" => self.description,
-          :"L_PAYMENTREQUEST_#{parent_index}_AMT#{index}" => Util.formatted_amount(self.amount),
-          :"L_PAYMENTREQUEST_#{parent_index}_QTY#{index}" => self.quantity,
-          :"L_PAYMENTREQUEST_#{parent_index}_ITEMCATEGORY#{index}" => self.category
+          "L_PAYMENTREQUEST_#{parent_index}_NAME#{index}"         => self.name,
+          "L_PAYMENTREQUEST_#{parent_index}_DESC#{index}"         => self.description,
+          "L_PAYMENTREQUEST_#{parent_index}_ITEMURL#{index}"      => self.url,
+          "L_PAYMENTREQUEST_#{parent_index}_AMT#{index}"          => Util.formatted_amount(self.amount),
+          "L_PAYMENTREQUEST_#{parent_index}_QTY#{index }"         => self.quantity,
+          "L_PAYMENTREQUEST_#{parent_index}_ITEMCATEGORY#{index}" => self.category
         }.delete_if do |k, v|
           v.blank?
         end
