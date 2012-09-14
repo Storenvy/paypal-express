ebebb6add6dca0da46ec06a82939d133f41c42ef
Corey Reece <corey@storenvy.com>
2012-09-14 15:28:23 -0700

    Adding more fields

--- lib/paypal/payment/request.rb
+++ lib/paypal/payment/request.rb
@@ -3,7 +3,7 @@ module Paypal
     class Request < Base
       attr_optional :action, :currency_code, :description, :notify_url, :billing_type,
         :billing_agreement_description, :billing_agreement_id, :seller_paypal_account_id,
-        :payment_request_id, :address
+        :payment_request_id, :address, :invoice_numbers, :note
       attr_accessor :amount, :items
 
       def initialize(attributes = {})
@@ -32,6 +32,7 @@ module Paypal
           :"PAYMENTREQUEST_#{index}_SHIPPINGAMT" => Util.formatted_amount(self.amount.shipping),
           :"PAYMENTREQUEST_#{index}_CURRENCYCODE" => self.currency_code,
           :"PAYMENTREQUEST_#{index}_DESC" => self.description,
+          :"PAYMENTREQUEST_#{index}_NOTETEXT" => self.note,
 
           #Address Fields
           "PAYMENTREQUEST_#{index}_SHIPTONAME"        => self.address[:name],
@@ -42,6 +43,9 @@ module Paypal
           "PAYMENTREQUEST_#{index}_SHIPTOCOUNTRYCODE" => self.address[:countrycode],
           "PAYMENTREQUEST_#{index}_SHIPTOPHONENUM"    => self.address[:phonenum],
           "PAYMENTREQUEST_#{index}_SHIPTOZIP"         => self.address[:zip],
+
+          "PAYMENTREQUEST_#{index}_INVNUM" => self.invoice_number,
+          "PAYMENTREQUEST_#{index}_NOTETEXT"      => self.note,
 
           # 3rd-party Recipient
           "PAYMENTREQUEST_#{index}_SELLERPAYPALACCOUNTID" => self.seller_paypal_account_id,
--- lib/paypal/payment/request/item.rb
+++ lib/paypal/payment/request/item.rb
@@ -1,7 +1,7 @@
 module Paypal
   module Payment
     class Request::Item < Base
-      attr_optional :name, :description, :amount, :quantity, :category, :url
+      attr_optional :name, :description, :amount, :tax, :quantity, :category, :url, :number
 
       def initialize(attributes = {})
         super
@@ -14,8 +14,11 @@ module Paypal
           "L_PAYMENTREQUEST_#{parent_index}_DESC#{index}"         => self.description,
           "L_PAYMENTREQUEST_#{parent_index}_ITEMURL#{index}"      => self.url,
           "L_PAYMENTREQUEST_#{parent_index}_AMT#{index}"          => Util.formatted_amount(self.amount),
-          "L_PAYMENTREQUEST_#{parent_index}_QTY#{index }"         => self.quantity,
-          "L_PAYMENTREQUEST_#{parent_index}_ITEMCATEGORY#{index}" => self.category
+          "L_PAYMENTREQUEST_#{parent_index}_TAXAMT#{index}"       => Util.formatted_amount(self.tax),
+          "L_PAYMENTREQUEST_#{parent_index}_QTY#{index}"          => self.quantity,
+          "L_PAYMENTREQUEST_#{parent_index}_ITEMCATEGORY#{index}" => self.category,
+          "L_PAYMENTREQUEST_#{parent_index}_NUMBER#{index}"       => self.number
+
         }.delete_if do |k, v|
           v.blank?
         end
